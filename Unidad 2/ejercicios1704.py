f9459f12d4ad5d9110246822d5115a9a19d0d896
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-04-17 09:40:33 -0400

    update

--- Unidad 2/ejercicios1704.py
+++ Unidad 2/ejercicios1704.py
@@ -7,3 +7,7 @@ x = input("Ingrese el valor de x: ")
 x = int(x) # convierte el str en  un entero para poder operar con él.
 resultado = (x**2 + 3*x + 1) / 4
 print(f"El resultado es: {resultado}")
+
+print("Nombre:\t\t\tJuan Lopez")
+print("Rut:\t\t\t11.111.111-1")
+print("Correo:\t\t\tjuan.lopez@gmail.com\nTelefono:\t\t99999999")
