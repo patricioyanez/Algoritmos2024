20c133e862275e2be05c97483efd1f77d0ac36ba
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-04-19 10:35:54 -0400

    update

---
+++ Unidad 2/EjercicioIngresoSala.py
@@ -0,0 +1,4 @@
+## ejercicios:
+# para controlar la entrada a la sala de clases
+# preguntar al alumno si esta matriculado
+# y si inscribio en la asignatura
--- Unidad 2/SentenciaSiAnd.py
+++ Unidad 2/SentenciaSiAnd.py
@@ -14,7 +14,3 @@ else:
     print("or: No ingreso los datos solicitados")
 
 
-## ejercicios:
-# para controlar la entrada a la sala de clases
-# preguntar al alumno si esta matriculado
-# y si inscribio en la asignatura
