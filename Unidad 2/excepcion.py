57660cb9c145971960b3418e129d31390f7ad108
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-04-23 09:52:36 -0400

    except

---
+++ Unidad 2/excepcion.py
@@ -0,0 +1,9 @@
+try:
+
+    numero = input("ingrese un numero:")
+    numero = int(numero)
+    print("El valor es:", numero )
+except:
+    print("El valor no puede ser convertido")
+
+print("fin de la aplicación")
