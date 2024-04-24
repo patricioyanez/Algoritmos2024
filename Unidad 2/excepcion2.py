e09df7596514a03cc6563f25513ea6a943fbc312
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-04-24 09:35:54 -0400

    update

--- Unidad 2/excepcion2.py
+++ Unidad 2/excepcion2.py
@@ -6,16 +6,20 @@ try:
     dividendo = int(dividendo)
     divisor = int(divisor)
     cociente = dividendo / divisor
-    print("El resultado es: ", cociente )
+    print("El resultado es: ", cociente1 )
+except NameError: # se Ejecuta cuando la variable no fue definida
+     print("La variable no existe")
 except ZeroDivisionError:
     print("No se puede dividir por cero")
 except TypeError:
     print("Ocurrio un error al concatenar")
 except ValueError:
     print("Ocurrio un error de conversión")
-else:
+except: # se ejecuta cuando la excepción NO esta definida
+    print("Error en alguna parte...")
+else: # se ejecuta cuando no hay excepción
     print("Operación realizada")
-finally:
+finally: # se ejecuta siempre
     print("Adios querido usuario.")
 
 print("****** Fin de aplicación ********")
