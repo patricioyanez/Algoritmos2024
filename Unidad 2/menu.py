8c8348d7aaebc5a79b9cb27e2a09dba34aae69fc
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-04-26 09:37:17 -0400

    Update menu.py

--- Unidad 2/menu.py
+++ Unidad 2/menu.py
@@ -8,13 +8,35 @@ while opcion != "0":
     print("2.- Restar")
     print("3.- Multiplicar")
     print("4.- Dividir")
+    print("5.- factor")
     print("0.- Salir")
 
-    opcion = input("Ingrese la opción")
+    opcion = input("Ingrese la opción: ")
 
-    if opcion not in ["0", "1", "2","3", "4"]:
+    if opcion not in ["0", "1", "2","3", "4", "5"]:
         print("La opción no es válida")
     else:
         # opciones correctas
         # pedir los numeros y verificar que sean int
-        
+        try:
+            numero1 = int(input("Ingrese primer valor   : "))
+            numero2 = int(input("Ingrese segundo valor  : "))
+        except:
+            print("Valor ingresado no es valido")
+            input("Presione enter para continuar....")
+            continue
+
+        if opcion == "1":
+            resultado = numero1 + numero2
+            print("El resultado es: ", resultado )
+        elif opcion == "2":
+            resultado = numero1 - numero2
+            print("El resultado es: ", resultado )
+        elif opcion == "3":
+            resultado = numero1 * numero2
+            print("El resultado es: ", resultado )
+        if opcion == "4":
+            resultado = numero1 / numero2
+            print("El resultado es: ", resultado )
+
+    input("Presione enter para continuar....")
