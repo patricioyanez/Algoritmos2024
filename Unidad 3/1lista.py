d18d0e7724a2420ab759faf74d9449cb7addaf77
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-05-13 09:17:35 -0400

    Colecciones

---
+++ Unidad 3/1lista.py
@@ -0,0 +1,44 @@
+nombres = [] # es una lista sin valores
+
+#lista con numeros
+numeros = [1,2,3,4,5,6,7]
+
+# acceder a un valor de la lista mediante
+# indice
+# el indice inicia del cero
+print("El numero es:", numeros[3])
+
+# recorrer
+for temporal in numeros:
+    print(temporal)
+
+# permite agregar elementos a la lista
+numeros.append(10)
+numeros.append(12)
+numeros.append(15)
+numeros.append("pashe pashe pashe")
+
+# mostrar todos los valores de la lista
+print("*** Mostrar nuevos valores ***")
+for temporal in numeros:
+    print(temporal)
+
+# ver contenido de la lista
+print(numeros)
+
+# eliminar un elemento
+print("=== Eliminar un elemento ===")
+numeros.remove("pashe pashe pashe")
+print(numeros)
+
+numeros.append(14)
+numeros.reverse()
+print(numeros)
+
+numeros.sort()
+print(numeros)
+
+# agregar un 8 y ordenar 
+# de mayor a menor
+
+
