e2f616cdbdddf06a4ba134cd8de69ae61c2e9574
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-05-22 09:50:31 -0400

    ejericico

---
+++ Unidad 3/4EjercicioLista4.py
@@ -0,0 +1,37 @@
+import os
+opcion = ""
+usuarios = []
+claves = []
+while opcion != "5":
+    os.system("cls")
+    print("=== Menú ===")
+    print("1.- Iniciar sesión")
+    print("2.- Registrar usuario")
+    print("3.- Eliminar usuario")
+    print("4.- Listar usuario")
+
+    print("5.- Salir")
+    opcion = input("Ingrese opción:")
+
+    if opcion not in ["1", "2", "3", "4", "5"]:
+        print("Opción no valida. Vuelva a intentar.")
+        input("Presione enter para continuar....")
+        continue
+
+    if opcion == "2":
+        print("*** Ingreso de usuarios ***")
+        usuario = input("Ingrese nombre de usuario:")
+        clave   = input("Ingrese clave del usuario:")
+        # strip elimina los espacios de los costados del string
+        if len(usuario.strip()) == 0 or len(clave.strip()) == 0:
+            print("Opción no valida. Vuelva a intentar.")
+            input("Presione enter para continuar....")
+            continue
+
+        usuarios.append(usuario)
+        claves.append(clave)
+    if opcion == "4":
+        for i in range(len(usuarios)):
+            print(f"Usuario: {usuarios[i]}, Clave: { claves[i]}")
+        input("Presione enter para continuar....")
+        continue
