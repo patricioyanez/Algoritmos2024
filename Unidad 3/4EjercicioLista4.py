9b1614c3af15049a6895a5990c60656ff47769dc
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-05-24 09:07:26 -0400

    update

--- Unidad 3/4EjercicioLista4.py
+++ Unidad 3/4EjercicioLista4.py
@@ -1,6 +1,6 @@
 import os
 opcion = ""
-usuarios = []
+usuarios = [] # si es una colección definir nombre en plural
 claves = []
 while opcion != "5":
     os.system("cls")
@@ -18,6 +18,23 @@ while opcion != "5":
         input("Presione enter para continuar....")
         continue
 
+    if opcion == "1":
+        print("|********* Login *********|")
+        usuario = input("Ingrese nombre de usuario:")
+        clave   = input("Ingrese clave del usuario:")
+        estaElUsuarioRegistrado = False
+        for indice in range(len(usuarios)):
+            if usuario == usuarios[indice]:
+                if clave == claves[indice]:
+                    estaElUsuarioRegistrado = True
+
+        if estaElUsuarioRegistrado:
+            print(f"=========> Bienvenido {usuario} :)")
+        else:
+            print("Error: Usuario y/o clave no son válidos")
+        
+        input("Presione enter para continuar....")
+
     if opcion == "2":
         print("*** Ingreso de usuarios ***")
         usuario = input("Ingrese nombre de usuario:")
@@ -30,6 +47,9 @@ while opcion != "5":
 
         usuarios.append(usuario)
         claves.append(clave)
+        print("El Usuario fue guardado con éxito")
+        input("Presione enter para continuar....")
+
     if opcion == "4": # len permite contar la cantidad de caracteres
         for i in range(len(usuarios)): # range es para entregar una secuencia numerica
             print(f"Usuario: {usuarios[i]}, Clave: { claves[i]}")
