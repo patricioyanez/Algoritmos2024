fee7e08450d7b897a708045d0badc55ace61b1c4
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-05-29 11:49:42 -0400

    update

---
+++ Unidad 3/tupla.py
@@ -0,0 +1,32 @@
+# crea una lista
+lista = ["academica", "administrativa", "contabilidad", "1", "1"]
+# crea una tupla
+tupla = ("academica", "administrativa", "contabilidad", "1", "1")
+
+print(lista)
+print(tupla)
+print(lista[1])
+print(tupla[1])
+
+# recorrer
+for valor in tupla:
+    print("valor: ",valor)
+
+miConjunto = {1,2,3,4,5,6, 1,2,3,4,5,6}
+print("**** Conjunto ****")
+
+# agregar elementos
+miConjunto.add(7)
+miConjunto.add(1)
+
+# eliminar elementos
+miConjunto.remove(5)
+miConjunto.pop()
+print(miConjunto)
+miConjunto.discard(10)
+
+for valor in miConjunto:
+    print("valor set: ",valor)
+
+
+# diccionario
