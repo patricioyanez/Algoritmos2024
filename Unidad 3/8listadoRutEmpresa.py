cd8d9928cff2bec6bd1b00033f61e7eab008ccd7
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-06-07 09:41:58 -0400

    update

--- Unidad 3/8listadoRutEmpresa.py
+++ Unidad 3/8listadoRutEmpresa.py
@@ -1,10 +1,11 @@
 import csv
 # lectura de archivo con formato CSV
-nuevosDatos = []
+nuevosDatos = [] # almacenara los datos con la nueva columna
 with open('8listadoRutEmpresa.csv', 'r', newline='') as documento:
     datosDocumento = csv.reader(documento)
 
     for fila in datosDocumento:
+        # agrega la nueva columna
         if int(fila[2]) < 100000000:
             fila.append("Pequeño Contribuyente")
         elif int(fila[2]) <= 200000000:
@@ -12,8 +13,10 @@ with open('8listadoRutEmpresa.csv', 'r', newline='') as documento:
         else:            
             fila.append("Gran Contribuyente")
         
+        # almacena la fila con la nueva columna
         nuevosDatos.append(fila)
 
+# crea y guarda los nuevos datos generados
 with open('8listadoRutEmpresa1.csv', 'w', newline='') as documento:
     escribir = csv.writer(documento)
     escribir.writerows(nuevosDatos)
