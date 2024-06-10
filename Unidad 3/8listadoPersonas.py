890a773d0eca308751eea16aff5e388df0005606
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-06-10 09:07:26 -0400

    Create 8listadoPersonas.py

---
+++ Unidad 3/8listadoPersonas.py
@@ -0,0 +1,8 @@
+# Ejercicios:
+# 
+# Crear una aplicación que permita:
+# 
+# Guardar el rut, nombre y año de nacimiento
+# Mostrar todos los datos almacenados
+# Calcular la edad de nacimiento del rut seleccionado y mostrar sus datos
+# salir
