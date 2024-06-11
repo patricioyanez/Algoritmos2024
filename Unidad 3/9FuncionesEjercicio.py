8f21919187d641005e40d8272df737a36d8883c0
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-06-11 09:43:39 -0400

    update

--- Unidad 3/9FuncionesEjercicio.py
+++ Unidad 3/9FuncionesEjercicio.py
@@ -20,15 +20,37 @@ def dividir(numero1, numero2):
     return numero1 / numero2
 
 while opcion != "5":
+    os.system("cls")
     print(""" **** Menú ****
           1.- Sumar
           2.- Restar
           3.- Multiplicar
           4.- Dividir
-          5.- Salir
-          """)
-    opcion = input("Ingrese opción")
+          5.- Salir""")
+    opcion = input("Ingrese opción: ")
 
     if opcion not in ['1','2','3','4','5']:
         print("Opción no válida")
         input("Presione enter para continuar...")
+    
+    if opcion == "5":
+        print("Aplicación Cerrada")
+        break
+
+    try:
+        numero1 = int(input("Ingrese 1er numero: "))
+        numero2 = int(input("Ingrese 2do numero: "))
+    except:
+        print("Numero no es valido")
+        input("Presione enter para continuar...")
+    resultado = 0
+    if opcion == "1":
+        resultado = sumar(numero1, numero2)
+    elif opcion == "2":
+        resultado = restar(numero1, numero2)
+    elif opcion == "3":
+        resultado = multiplicar(numero1, numero2)
+    elif opcion == "4":
+        resultado = dividir(numero1, numero2)
+    print("El resultado es:", resultado)
+    input("Presione enter para continuar...")
