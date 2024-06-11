021504c291437ed6790cb37f63a91e17f4f92ae1
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-06-11 09:38:00 -0400

    update

--- Unidad 3/9FuncionesEjercicio.py
+++ Unidad 3/9FuncionesEjercicio.py
@@ -3,3 +3,32 @@
 # devolver el resultado y mostrar en pantalla
 # las operaciones a usar son:
 # +,-,* y /
+
+import os
+opcion = ""
+
+def sumar(numero1, numero2):
+    return numero1 + numero2
+def restar(numero1, numero2):
+    return numero1 - numero2
+def multiplicar(numero1, numero2):
+    return numero1 * numero2
+def dividir(numero1, numero2):
+    if numero2 == 0:
+        print("Error, no se puede dividir por cero")
+        return 0
+    return numero1 / numero2
+
+while opcion != "5":
+    print(""" **** Menú ****
+          1.- Sumar
+          2.- Restar
+          3.- Multiplicar
+          4.- Dividir
+          5.- Salir
+          """)
+    opcion = input("Ingrese opción")
+
+    if opcion not in ['1','2','3','4','5']:
+        print("Opción no válida")
+        input("Presione enter para continuar...")
