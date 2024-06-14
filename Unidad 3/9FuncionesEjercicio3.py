e5239a876c6f0b36a2bbc3463736959a7ea83759
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-06-14 09:02:12 -0400

    update

---
+++ Unidad 3/9FuncionesEjercicio3.py
@@ -0,0 +1,10 @@
+# Crear una aplicación que permita registrar los ingresos a un recinto
+# para el ingreso se requiere el rut, validar si esta bueno, antes de permitir
+# el ingreso
+# todos las personas deben registrar su rut, nombre y edad. Si existe dar la bienvenida 
+# mencionando su nombre.
+# los datos deben ser almacenados en un archivo .csv
+# Otras opciones a realizar: 
+#   listar las personas que han ingresado.
+#   Creación de archivo
+#   Mostrar estadisticas según rango etario que ha entrado al recinto.
