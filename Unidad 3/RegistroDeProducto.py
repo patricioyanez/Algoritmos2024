64f78232efad3b3e8c069aeae4a1e634c00bdff1
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-06-24 08:46:14 -0400

    update

---
+++ Unidad 3/RegistroDeProducto.py
@@ -0,0 +1,12 @@
+# Crear una app, que permita registrar productos de una tienda de abarrotes
+# Los requisitos son:
+# crear menu con las siguientes opciones:
+# 
+# Crear archivo con formato csv
+# Ingresar productos (codigo de barra, nombre, marca, precio, stock)
+# Listar productos
+# Eliminar producto
+# Modificar stock actual
+# 
+# Usar funciones, listas y librerias vistas en clases.
+# Crear repositorio en github y guardar los cambios
